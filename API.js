f83df794856442492a733c7af2c3267f08da27bd
Akash-Chhangani <akashchhangani4@gmail.com>
2024-02-08 12:43:51 +0530

    Access button and paragraph to show hoe API fetch the data using JSON method

--- API.js
+++ API.js
@@ -1,7 +1,13 @@
 const URL ="https://cat-fact.herokuapp.com/facts";
+const factPara = document.querySelector("#fact");
+const button = document.querySelector("#btn");
 
 const getFats = async ()=>{
     console.log("Getting Data.......");
     let response = await fetch(URL);
-    console.log(response);
-}
+    console.log(response);//JSON Format
+    let data = await response.json();
+    factPara.innerText = data[0].text;
+};
+
+button.addEventListener("click",getFats);
