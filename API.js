73f5763696529cb081e5cbfc0058c5ad0a246979
Akash-Chhangani <akashchhangani4@gmail.com>
2024-02-08 12:23:48 +0530

    add API file to index.html for execution

--- API.js
+++ API.js
@@ -1,1 +1,7 @@
 const URL ="https://cat-fact.herokuapp.com/facts";
+
+const getFats = async ()=>{
+    console.log("Getting Data.......");
+    let response = await fetch(URL);
+    console.log(response);
+}
