49c2ffc4a2142727759b05723b4dd07dfde43625
Akash Chhangani <156087593+Akash-Chhangani@users.noreply.github.com>
2024-07-25 10:18:59 +0530

    Customize our React Query Properties and Manipulate them

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,16 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.jsx";
 import "./index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 5,
+      staleTime: 6 * 1000,
+      gcTime: 600_000,
+      // many more
+    },
+  },
+});
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
